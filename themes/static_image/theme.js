9c6658d1de0464655bd0ae02ca7cc9faea946956
Bruno St. John <brunost.john@icloud.com>
2023-01-02 08:42:08 +0000

    did some stuff

--- themes/static_image/theme.js
+++ themes/static_image/theme.js
@@ -1,9 +1,21 @@
 const path = require("path");
 const fs = require("fs");
+const sizeOf = require("image-size");
+const mime = require("mime-types");
 
 let config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
 
+let dimensions = sizeOf(config.imagePath);
+
 let imageBase64 = fs.readFileSync(config.imagePath, {encoding: "base64"});
+
+//TODO: fix this
+if(!(dimensions.width == 480 && dimensions.height == 480)) {
+    let imageBase64 = fs.readFileSync(path.join(__dirname, "fallback.jpeg"), {encoding: "base64"});
+}
+if(mime.lookup(config.imagePath) != "image/jpeg") {
+    let imageBase64 = fs.readFileSync(path.join(__dirname, "fallback.jpeg"), {encoding: "base64"});
+}
 
 function renderFrame() {
     return imageBase64;
