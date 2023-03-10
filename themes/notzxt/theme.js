fd506f46d3a4a821caae35ce3d51c161cfb9ac9b
Bruno St. John <brunost.john@icloud.com>
2023-03-10 00:45:52 +0000

    moaaaaaar slideeeeeeeeey

--- themes/notzxt/theme.js
+++ themes/notzxt/theme.js
@@ -50,6 +50,18 @@ const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
 context.textAlign = "center";
 
+let oldVal1 = 30;
+let oldVal2 = 30;
+let transitionState1 = false;
+let transitionState2 = false;
+const framesUntilDone = Math.round((config.refreshFrequency / 2000) * 25);
+let currentCounter1 = 0;
+let currentCounter2 = 0;
+let arcIncrements1 = { part1: 0, part2: 0 };
+let arcVals1 = { part1: 0, part2: 0 };
+let arcIncrements2 = { part1: 0, part2: 0 };
+let arcVals2 = { part1: 0, part2: 0 };
+
 function renderFrame() {
   sensorValue1 = sensors.rateLimitedGetSensorValueByPath(
     config.sensorPath1,
@@ -77,48 +89,149 @@ function renderFrame() {
     sensorValueCalc2 = sensorValue2;
   }
 
+  if (
+    sensorValueCalc1 != oldVal1 &&
+    !transitionState1 &&
+    config.smoothAnimation
+  ) {
+    currentCounter1 = framesUntilDone;
+    transitionState1 = true;
+    arcVals1.part1 = scale(oldVal1, 30, 85, 0.9, 0.6);
+    arcVals1.part2 = scale(oldVal1, 30, 85, 1.1, 1.4);
+
+    arcIncrements1.part1 =
+      (scale(sensorValueCalc1, 30, 85, 0.9, 0.6) -
+        scale(oldVal1, 30, 85, 0.9, 0.6)) /
+      framesUntilDone;
+    arcIncrements1.part2 =
+      (scale(sensorValueCalc1, 30, 85, 1.1, 1.4) -
+        scale(oldVal1, 30, 85, 1.1, 1.4)) /
+      framesUntilDone;
+    oldVal1 = sensorValueCalc1;
+  }
+
+  if (
+    sensorValueCalc2 != oldVal2 &&
+    !transitionState2 &&
+    config.smoothAnimation
+  ) {
+    currentCounter2 = framesUntilDone;
+    transitionState2 = true;
+
+    arcIncrements2.part1 =
+      (scale(sensorValueCalc2, 30, 85, 1.9, 1.6) -
+        scale(oldVal2, 30, 85, 1.9, 1.6)) /
+      framesUntilDone;
+    arcIncrements2.part2 =
+      (scale(sensorValueCalc2, 30, 85, 2.1, 2.4) -
+        scale(oldVal2, 30, 85, 2.1, 2.4)) /
+      framesUntilDone;
+    oldVal2 = sensorValueCalc2;
+  }
+
   context.clearRect(0, 0, 480, 480);
 
-  context.fillStyle = config.backgroundColour;
-  context.fillRect(0, 0, width, height);
+  if (transitionState1 && config.smoothAnimation) {
+    arcVals1.part1 += arcIncrements1.part1;
+    arcVals1.part2 += arcIncrements1.part2;
 
-  context.beginPath();
-  context.arc(
-    width / 2,
-    height / 2,
-    200,
-    scale(sensorValueCalc1, 30, 85, 0.9, 0.6) * Math.PI,
-    scale(sensorValueCalc1, 30, 85, 1.1, 1.4) * Math.PI
-  );
-  context.strokeStyle = config.colour1;
-  context.fillStyle = config.colour1;
-  context.font = "40px Gotham-SSM";
-  context.fillText(config.sensorText1, 150, 300);
-  context.font = "70px Gotham-SSM";
-  context.fillStyle = config.textColour;
-  context.fillText(Math.round(sensorValue1) + "°", 168, 250);
-  context.lineWidth = 40;
-  context.lineCap = "round";
-  context.stroke();
+    context.fillStyle = config.backgroundColour;
+    context.fillRect(0, 0, width, height);
+    context.beginPath();
+    context.arc(
+      width / 2,
+      height / 2,
+      200,
+      arcVals1.part1 * Math.PI,
+      arcVals1.part2 * Math.PI
+    );
+    context.strokeStyle = config.colour1;
+    context.fillStyle = config.colour1;
+    context.font = "40px Gotham-SSM";
+    context.fillText(config.sensorText1, 150, 300);
+    context.font = "70px Gotham-SSM";
+    context.fillStyle = config.textColour;
+    context.fillText(Math.round(sensorValue1) + "°", 168, 250);
+    context.lineWidth = 40;
+    context.lineCap = "round";
+    context.stroke();
 
-  context.beginPath();
-  context.arc(
-    width / 2,
-    height / 2,
-    200,
-    scale(sensorValueCalc2, 30, 85, 1.9, 1.6) * Math.PI,
-    scale(sensorValueCalc2, 30, 85, 2.1, 2.4) * Math.PI
-  );
-  context.strokeStyle = config.colour2;
-  context.fillStyle = config.colour2;
-  context.font = "40px Gotham-SSM";
-  context.fillText(config.sensorText2, 330, 300);
-  context.font = "70px Gotham-SSM";
-  context.fillStyle = config.textColour;
-  context.fillText(Math.round(sensorValue2) + "°", 348, 250);
-  context.lineWidth = 40;
-  context.lineCap = "round";
-  context.stroke();
+    currentCounter1--;
+    if (currentCounter1 == 0) {
+      transitionState1 = false;
+      arcIncrements1 = { part1: 0, part2: 0 };
+    }
+  } else {
+    context.fillStyle = config.backgroundColour;
+    context.fillRect(0, 0, width, height);
+    context.beginPath();
+    context.arc(
+      width / 2,
+      height / 2,
+      200,
+      scale(sensorValueCalc1, 30, 85, 0.9, 0.6) * Math.PI,
+      scale(sensorValueCalc1, 30, 85, 1.1, 1.4) * Math.PI
+    );
+    context.strokeStyle = config.colour1;
+    context.fillStyle = config.colour1;
+    context.font = "40px Gotham-SSM";
+    context.fillText(config.sensorText1, 150, 300);
+    context.font = "70px Gotham-SSM";
+    context.fillStyle = config.textColour;
+    context.fillText(Math.round(sensorValue1) + "°", 168, 250);
+    context.lineWidth = 40;
+    context.lineCap = "round";
+    context.stroke();
+  }
+
+  if (transitionState2 && config.smoothAnimation) {
+    arcVals2.part1 += arcIncrements2.part1;
+    arcVals2.part2 += arcIncrements2.part2;
+
+    context.beginPath();
+    context.arc(
+      width / 2,
+      height / 2,
+      200,
+      arcVals2.part1 * Math.PI,
+      arcVals2.part2 * Math.PI
+    );
+    context.strokeStyle = config.colour2;
+    context.fillStyle = config.colour2;
+    context.font = "40px Gotham-SSM";
+    context.fillText(config.sensorText2, 330, 300);
+    context.font = "70px Gotham-SSM";
+    context.fillStyle = config.textColour;
+    context.fillText(Math.round(sensorValue2) + "°", 348, 250);
+    context.lineWidth = 40;
+    context.lineCap = "round";
+    context.stroke();
+
+    currentCounter2--;
+    if (currentCounter2 == 0) {
+      transitionState2 = false;
+      arcIncrements2 = { part1: 0, part2: 0 };
+    }
+  } else {
+    context.beginPath();
+    context.arc(
+      width / 2,
+      height / 2,
+      200,
+      scale(sensorValueCalc2, 30, 85, 1.9, 1.6) * Math.PI,
+      scale(sensorValueCalc2, 30, 85, 2.1, 2.4) * Math.PI
+    );
+    context.strokeStyle = config.colour2;
+    context.fillStyle = config.colour2;
+    context.font = "40px Gotham-SSM";
+    context.fillText(config.sensorText2, 330, 300);
+    context.font = "70px Gotham-SSM";
+    context.fillStyle = config.textColour;
+    context.fillText(Math.round(sensorValue2) + "°", 348, 250);
+    context.lineWidth = 40;
+    context.lineCap = "round";
+    context.stroke();
+  }
 
   if (config.showEmblem) context.drawImage(emblem, 220, 110);
 
@@ -126,6 +239,8 @@ function renderFrame() {
 }
 
 function renderPreview() {
+  currentCounter1 = 0;
+  currentCounter2 = 0;
   sensors = new Sensors();
   reloadConfig();
   return renderFrame();
@@ -190,6 +305,12 @@ module.exports = {
       showEmblem: {
         type: "bool",
         title: "Show emblem",
+        defaultValue: true,
+      },
+      smoothAnimation: {
+        type: "bool",
+        title:
+          "Smooth transitions in value indicator (warning: increases CPU usage)",
         defaultValue: true,
       },
     },
