79d2c49c36c22df7809566f43f5d2ac1f9f592b9
Bruno St. John <brunost.john@icloud.com>
2023-03-14 12:50:32 +0000

    updated const

--- devices-src/kraken/src/statics.rs
+++ devices-src/kraken/src/statics.rs
@@ -3,4 +3,5 @@ pub mod statics {
     pub const BULK_WRITE_LENGTH: usize = 512;
     pub const BULK_ENDPOINT: u8 = 0x02;
     pub const TEN_MS: std::time::Duration = std::time::Duration::from_millis(10);
+    pub const BULK_INTERFACE: u8 = 0x00;
 }
