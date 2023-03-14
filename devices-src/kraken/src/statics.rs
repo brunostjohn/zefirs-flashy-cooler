fe923fe3e574343a4741b46ec393441a8d717d12
Bruno St. John <brunost.john@icloud.com>
2023-03-14 04:43:30 +0000

    wrong endpoint

--- devices-src/kraken/src/statics.rs
+++ devices-src/kraken/src/statics.rs
@@ -1,6 +1,6 @@
 pub mod statics {
     pub const WRITE_LENGTH: usize = 64;
     pub const BULK_WRITE_LENGTH: usize = 512;
-    pub const BULK_ENDPOINT: u8 = 0x00;
+    pub const BULK_ENDPOINT: u8 = 0x02;
     pub const TEN_MS: std::time::Duration = std::time::Duration::from_millis(10);
 }
