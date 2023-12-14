09a9f37d7ccf56c9d88d9b1b9f52510bb4466f13
Bruno St John <bruno@acornlabs.me>
2023-12-14 22:02:17 +0000

    feat: this is evil

--- crates/ultralight/src/js/object.rs
+++ crates/ultralight/src/js/object.rs
@@ -1,6 +1,10 @@
-use super::types::{IsJSValue, JSObject, JSOpaque};
-use crate::value::JSValue;
-use std::ptr;
+use super::{
+    internal_string::JSCString,
+    types::{IsJSValue, JSObject, JSOpaque},
+};
+use crate::{context::JSContext, value::JSValue};
+use libffi::high::Closure6;
+use std::{mem, ptr};
 
 impl<'a> JSValue<JSObject<'a>> {
     pub fn set_property<K: IsJSValue, V: IsJSValue>(&mut self, key: JSValue<K>, value: JSValue<V>) {
@@ -28,6 +32,86 @@ impl<'a> JSValue<JSObject<'a>> {
 
         JSValue::new(JSOpaque {
             internal: value,
+            guard: self.internal.guard,
+        })
+    }
+
+    pub fn make_function<'b: 'a, S, F, V, E>(
+        &mut self,
+        name: S,
+        callback: F,
+    ) -> JSValue<JSObject<'a>>
+    where
+        S: AsRef<str>,
+        F: (Fn(
+                JSValue<JSObject<'_>>,
+                JSValue<JSObject<'_>>,
+                &[JSValue<JSOpaque<'_>>],
+            ) -> Result<JSValue<V>, JSValue<E>>)
+            + 'b,
+        V: IsJSValue,
+        E: IsJSValue,
+    {
+        let name = JSCString::new(name, self.internal.guard);
+        let wrapped_function = move |ctx: ultralight_sys::JSContextRef,
+                                     function: ultralight_sys::JSObjectRef,
+                                     this: ultralight_sys::JSObjectRef,
+                                     argument_count: usize,
+                                     arguments: *const ultralight_sys::JSValueRef,
+                                     exception: *mut ultralight_sys::JSValueRef|
+              -> ultralight_sys::JSValueRef {
+            let evil_ctx =
+                JSContext::new(ctx, unsafe { mem::transmute(ptr::null::<u8>()) }, unsafe {
+                    mem::transmute(ptr::null::<u8>())
+                });
+            let arguments = unsafe { std::slice::from_raw_parts(arguments, argument_count) };
+            let arguments = arguments
+                .iter()
+                .map(|value| {
+                    JSValue::new(JSOpaque {
+                        internal: *value,
+                        guard: &evil_ctx,
+                    })
+                })
+                .collect::<Vec<_>>();
+            let this = JSValue::new(JSObject {
+                internal: this,
+                guard: &evil_ctx,
+            });
+            let function = JSValue::new(JSObject {
+                internal: function,
+                guard: &evil_ctx,
+            });
+            let result = callback(this, function, arguments.as_slice());
+            match result {
+                Ok(value) => value.internal.get_value(),
+                Err(value) => {
+                    unsafe {
+                        *exception = value.internal.get_value();
+                    }
+                    ptr::null_mut()
+                }
+            }
+        };
+        let closure = Closure6::new(&wrapped_function);
+        let code_ptr: &'b unsafe extern "C" fn(
+            ultralight_sys::JSContextRef,
+            ultralight_sys::JSObjectRef,
+            ultralight_sys::JSObjectRef,
+            usize,
+            *const ultralight_sys::JSValueRef,
+            *mut ultralight_sys::JSValueRef,
+        ) -> ultralight_sys::JSValueRef = unsafe { mem::transmute(closure.code_ptr()) };
+        let function = unsafe {
+            ultralight_sys::JSObjectMakeFunctionWithCallback(
+                **self.internal.guard,
+                *name,
+                Some(*code_ptr),
+            )
+        };
+
+        JSValue::new(JSObject {
+            internal: function,
             guard: self.internal.guard,
         })
     }
